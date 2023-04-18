409c088fb2ad99fabc6053a083c35dce52c52abd
Rafael Santos <rafa.05125@gmail.com>
2023-04-18 09:39:02 -0300

    Solution with OOP

---
+++ Triangle/Triangulo.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Triangle
+{
+    class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+    }
+}
